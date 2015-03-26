90f59488039eb821189de12d1285e10fc2d55c85
hhjiang13@gmail.com <hhjiang13@gmail.com>
2015-03-26 10:30:03 +0800

    add

---
+++ MS/js/controller.js
@@ -0,0 +1,94 @@
+/*
+2015.3.22 by hhjiang email:hhjiang13@gmail
+this js is for all the controller
+*/
+
+// we don't support IE with lower than 9, so here is the check  
+$(function () { 
+	var brow=navigator.userAgent.indexOf('MSIE');
+    if(brow>0){
+    	var browVer = Number(navigator.userAgent.substring(brow+5, brow+8));
+    	if(browVer <= 8){
+    		document.body.innerHTML="";
+    		var infoDiv = document.createElement('div');
+    		infoDiv.innerHTML="为了您的更好体验，请更换浏览器或者升级IE浏览器";
+    		infoDiv.style.margin="20% 0 0 35%";
+    		document.body.appendChild(infoDiv);
+    	}
+    }
+
+
+    function removeEventHandler(target, type, func) {
+        if (target.removeEventListener){
+            //监听IE9，谷歌和火狐
+            target.removeEventListener(type, func, false);
+        } else if (target.detachEvent){
+            target.detachEvent("on" + type, func);
+        }else {
+            delete target["on" + type];
+        }
+    }
+    //addEventHandler(bindEventBtn,"click",eventTwo );
+    //removeEventHandler(bindEventBtn,"click",eventOne);
+
+    //for the nav
+    $("[data-toggle='popover']").popover();
+	$('#myTab a:last').tab('show')
+
+    //this is for img record 
+     numRec = 1;
+     //showBit if 1 the clock is work, 0 is not
+     showBit = 1;
+     //initial the pic
+    for (var i = IMGNUM; i >= 1; i--) {
+        drawimage(i);
+    };
+    changeIMG();
+    imgClock = self.setInterval(function () {
+        changeIMG();
+    }, 4000);
+
+    //change the screen
+    window.onresize = function ()
+    {
+        WIDTH = document.documentElement.clientWidth;
+        HEIGHT = document.documentElement.clientHeight;
+        if (WIDTH * HEIGHT == 0)
+        {
+            WIDTH = document.body.clientWidth;
+            HEIGHT = document.body.clientWidth;
+        }
+        for (var i = IMGNUM; i >= 1; i--) {
+            drawimage(i);
+        };
+    };
+
+
+    //bind the event for left and right 
+    $(".iconImgL").click(function(){changeLR(-1)});
+    $(".iconImgR").click(function(){changeLR(1)});
+    $('#tabSet button').on('click', stopImg);
+    $('.navbar-header').on('click', reloadImg);
+
+
+    //for the recommend page
+    $(".specialH").hover(
+          function () {
+            $(this).css('display','none').fadeOut(1000);
+            $(this).siblings('.specialC').css('display','block').fadeIn(1000);
+          },
+          function () {
+          }
+        );
+
+    $(".specialC").hover(
+          function () {
+            
+          },
+          function () {
+            $(this).css('display','none').fadeOut(1000);
+            $(this).siblings('.specialH').css('display','block').fadeIn(1000);
+          }
+        );
+
+});
---
+++ MS/js/view.js
@@ -0,0 +1,79 @@
+//this is for display
+IMGNUM = 3;
+//decription for all the pic in the home page,each page has two items, h1 and p, please attention.
+DESC = ["旧金山高端富人区公寓-Toorak Park, Armadale","首付3.9万澳币起，坐落在布里斯班中央商业区，坐拥俯瞰城市全景，布里斯班河景，故事桥和Coot-tha山景。","https://www.baidu.com/","旧金山高端富人区公寓","首付3.9万澳币起，坐落在布里斯班中央商业区，坐拥俯瞰城市全景，布里斯班河景，故事桥和Coot-tha山景。","https://www.baidu.com/","旧金山高端富人区","首付3.9万澳币起，坐落在布里斯班中央商业区，坐拥俯瞰城市全景，布里斯班河景，故事桥和Coot-tha山景。","http://www.sohu.com/"];
+WIDTH = document.documentElement.clientWidth;
+HEIGHT = document.documentElement.clientHeight;
+if (WIDTH * HEIGHT == 0)
+{
+    WIDTH = document.body.clientWidth;
+    HEIGHT = document.body.clientWidth;
+}
+
+function drawimage(imgIndex){
+	var mycanvas = document.getElementById("myCanvas"+imgIndex);
+	mycanvas.width = WIDTH;
+    mycanvas.height = HEIGHT*0.85;
+}
+
+
+function changeIMG(){
+  //  gurantee numRec ++ using bibao
+  if(showBit===1){ 
+      numRec++;
+    var preNum = numRec-1;
+    if (preNum <= 0) {preNum = IMGNUM};
+    var preImg = document.getElementById("myCanvas"+preNum);
+    preImg.className = 'a-fadeoutL';
+    if (numRec>IMGNUM) {numRec=1;};
+    var nextImg = document.getElementById("myCanvas"+numRec);
+    nextImg.className = 'a-fadein';
+    $('#description').children('h2').text(DESC[numRec*3 - 3]);
+    $('#description').children('span').text(DESC[numRec*3-2]);
+    $('#description').children('.infoHref')[0].href = DESC[numRec*3 - 1];
+  }
+}
+
+// for the left and right
+function changeLR(){
+    showBit = 0;
+    if (numRec===0) {numRec=1;};
+    var preNum = numRec;
+    var preImg = document.getElementById("myCanvas"+preNum);
+    preImg.className = 'a-fadeoutL';
+    if(isNaN(arguments[-1])){
+          this.length = 0;
+     }
+    numRec = numRec + arguments[0];
+    if (numRec>IMGNUM) {
+        numRec=1;
+    }else if(numRec<=0){
+        numRec=IMGNUM;
+    };
+    var nextImg = document.getElementById("myCanvas"+numRec);
+    nextImg.className = 'a-fadein';
+}
+
+//for other tabs to hide the img
+// function hideImg(){
+//     showBit = 0;
+//     for (var i = IMGNUM; i >= 1; i--) {
+//         document.getElementById("myCanvas"+i).className = 'a-fadeout';
+//     };
+//         //this guarantee the display of
+//     $(".home").className="tab-pane";
+//     $()
+// }
+
+//for the home 
+function reloadImg(){
+    showBit = 1;
+    if(numRec===0){numRec=1;}
+    document.getElementById("myCanvas"+numRec).className = 'a-fadein';
+}
+
+//for login
+function stopImg(){
+    showBit = 0;
+    console.log(showBit);
+}
